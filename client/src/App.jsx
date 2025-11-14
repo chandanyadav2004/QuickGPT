997b727a51f33f15c26204fbd5d9025653569cd0
chandanyadav2004 <demo8788151949@gmail.com>
2025-11-14 20:20:13 +0530

    SideBar and navigate completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './components/Sidebar'
 import ChatBox from './components/ChatBox'
 import Credits from './pages/Credits'
 import { Route, Routes } from 'react-router-dom'
 import Community from './pages/Community'
+import { assets } from './assets/assets'
     
 
 
 const App = () => {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
   return (
     <>
+    {
+      !isMenuOpen && <img src={assets.menu_icon} className=' absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={ () => setIsMenuOpen(true)} alt="" />
+    }
     <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white'>
       <div className=' flex h-screen w-screen'>
-        <Sidebar />
+        <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
         <Routes>
           <Route path='/' element={<ChatBox />} />
-          <Route path='/credit' element={<Credits />} />
+          <Route path='/credits' element={<Credits />} />
           <Route path='/community' element={<Community />} />
           
           
