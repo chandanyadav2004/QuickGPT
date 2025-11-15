6c652116a0071b602f40956a4a96d1aeb777b02b
chandanyadav2004 <demo8788151949@gmail.com>
2025-11-15 19:35:22 +0530

    creditRouter access point

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js'
 import userRouter from './routes/userRoutes.js'
 import chatRouter from './routes/chatRoute.js'
 import messageRouter from './routes/messageRoutes.js'
+import creditRouter from './routes/creditsRoutes.js'
 
 const app = express()
 
@@ -20,7 +21,8 @@ app.use(express.json())
 app.get('/', (req,res)=> res.send('Server is Live!'))
 app.use('/api/user', userRouter);
 app.use('/api/chat/',chatRouter);
-app.use('/api/message',messageRouter)
+app.use('/api/message',messageRouter);
+app.use('/api/credit', creditRouter)
 
 const port = process.env.port || 3000
 
