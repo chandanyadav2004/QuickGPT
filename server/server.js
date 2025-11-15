e91f145d1987de327359759b6803c7a4a953f47b
chandanyadav2004 <demo8788151949@gmail.com>
2025-11-15 12:12:58 +0530

    server routes and working

---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'dotenv/config'
+import cors from 'cors'
+import connectDB from './config/db.js'
+import userRouter from './routes/userRoutes.js'
+import chatRouter from './routes/chatRoute.js'
+
+const app = express()
+
+await connectDB()
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// routes
+
+app.get('/', (req,res)=> res.send('Server is Live!'))
+app.use('/api/user', userRouter);
+app.use('/api/chat/',chatRouter)
+
+const port = process.env.port || 3000
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`)})
